0b8ca6b32552d3ee7c5758b226eb0418c2bfc944
D-Raff <damonraffels@gmail.com>
2023-11-14 09:56:27 +0200

    added joel's examples

--- script/code.js
+++ script/code.js
@@ -1,32 +1,75 @@
-const btnGrade = document.querySelector("[data-grade]");
-const Output = document.querySelector("[data-res]");
+// const btnGrade = document.querySelector("[data-grade]");
+// const Output = document.querySelector("[data-res]");
 
-function Grade() {
-  let marks = +document.querySelector("[data-marks]").value
+// function Grade() {
+//   let marks = +document.querySelector("[data-marks]").value
+
+//   switch (true) {
+//     case marks === 100:
+//       Output.textContent = "A+";
+//     break;
+//     case marks >= 90 && marks <= 99:
+//         Output.textContent = "A";
+//     break;
+//     case marks >= 80 && marks <= 89:
+//        Output.textContent = "B+";
+//     break;
+//     case marks >= 70 && marks <= 79:
+//        Output.textContent = "Distinction";
+//     break;
+//     case marks >= 60 && marks <= 69:
+//        Output.textContent = "C+";
+//     break;
+//     case marks >= 50 && marks <= 59:
+//        Output.textContent = "passed";
+//     break;
+//     case marks <= 49:
+//        Output.textContent = "passed";
+//     break;
+//     default:
+//         Output.textContent = "Failed";
+//     break;
+//   }
+// }
+
+// btnGrade.addEventListener("click", Grade);
+
+// Joel's way of doing it
+
+let btnSubmit = document.querySelector("[data-grade]");
+
+function displayMarks() {
+  let marks = +document.querySelector("[data-marks]").value;
+
+  let Output = document.querySelector("[data-res]");
 
   switch (true) {
-    case marks === 100:
-      Output.textContent = "A+";
-    break;
-    case marks >= 90 && marks <= 99:
-        Output.textContent = "A";
-    break;
-    case marks >= 80 && marks <= 89:
-       Output.textContent = "B+";
-    break;
-    case marks >= 70 && marks <= 79:
-       Output.textContent = "Distinction";
-    break;
-    case marks >= 60 && marks <= 69:
-       Output.textContent = "C+";
-    break;
-    case marks >= 50 && marks <= 59:
-       Output.textContent = "passed";
-    break;
+    case marks < 50:
+      Output.textContent = `sorry, you failed. try again`;
+      break;
+    case marks <= 59:
+      Output.textContent = `you passed`;
+      break;
+    case marks <= 69:
+      Output.textContent = `C+`;
+      break;
+    case marks <= 79:
+      Output.textContent = `Distinction`;
+      break;
+    case marks <= 89:
+      Output.textContent = `B+`;
+      break;
+    case marks <= 99:
+      Output.textContent = `A`;
+      break;
+    case marks == 100:
+      Output.textContent = `A+`;
+      break;
+
     default:
-        Output.textContent = "Failed";
-    break;
+      Output.textContent = `you are a ghost`;
+      break;
   }
 }
 
-btnGrade.addEventListener("click", Grade);
+btnSubmit.addEventListener("click", displayMarks);
